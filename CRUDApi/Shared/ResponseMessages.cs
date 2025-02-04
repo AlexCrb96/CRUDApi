4f1857363c978bd5b1a3a054eef4a798723ed056
Alexandru Carbunaru <alex.carbunaru@gmail.com>
2025-02-04 23:05:41 +0200

    Refactored repeated output messages into a dedicated ResponseMessages class for reuse

---
+++ CRUDApi/Shared/ResponseMessages.cs
@@ -0,0 +1,11 @@
+﻿namespace CRUDApi.Shared
+{
+    public static class ResponseMessages
+    {
+        public const string NothingFound = "No {0} found.";
+        public const string IdNotFound = "{0} with ID {1} does not exist.";
+        public const string InputNotValid = "{0} data is invalid.";
+        public const string OneOrMoreNotFound = "One or more {0} not found.";
+        public const string FailedToCreate = "Failed to create {0}.";
+    }
+}
